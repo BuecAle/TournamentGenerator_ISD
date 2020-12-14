98f3d4502910cf1581c317fe3d134c425e895b0c
lucyh <lucyhgannon@gmail.com>
2020-12-14 14:16:18 +0000

    Max no. of Char

--- trunk/TournamentGenerator/TournamentGenerator/TournamentGenerator/settings.py
+++ trunk/TournamentGenerator/TournamentGenerator/TournamentGenerator/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'TournamentGenerator.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'new_db.sqlite3'),
     }
 }
 
